316e51129c2d910e26ad1befe925c9afbed3ad60
unknown <alpeshjikadra@yahoo.com>
2017-01-08 22:37:57 +0530

    Initial commit for junit5 practice

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class SimpleTest {
+
+	@Test
+	@DisplayName("XXX")
+	void simpleTest(){
+		Assertions.assertTrue(!false, "Answer must be true");
+	}
+}
